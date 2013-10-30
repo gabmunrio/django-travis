b99f68485e881cdb2f0653e5d704f7f87016bfb6
gabmunrio <munozrios@gmail.com>
2013-10-30 17:11:25 +0100

    Trasteando con mas pruebas de test

--- django/mysite/polls/tests.py
+++ django/mysite/polls/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from polls.models import Choice
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,8 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class ChoiceMethodTest(TestCase):
+	def test_choice_negative(self):
+		choice = Choice(votes=1)
+		self.assertEqual(choice.votes > 0, True)
